4ad4eb62e747a17b7ab476df21ac4e37611593c2
Yacin Bahi <yacin@red64.io>
2026-02-11 09:47:12 +0100

    fix: profile issue

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,6 +3,11 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 900 ]
 
+  setup do
+    # Reset viewport to desktop size before each test
+    page.driver.browser.manage.window.resize_to(1400, 900)
+  end
+
   def sign_in_as_system(user)
     visit new_session_path
     fill_in "Email address", with: user.email_address
